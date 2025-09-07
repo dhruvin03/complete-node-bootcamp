6730d44b320a27075d8b2aaef4e2620392be312d
Dhruvin <dhruvin@marketsmojo.com>
2025-09-07 16:04:04 +0530

    Addig Error Validations for out natours app

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
 
@@ -36,5 +38,15 @@ app.use((req, res, next) => {
 /***** Routes *****/
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  //let err = new Error(`Cannot find ${req.originalUrl} on this server!`);
+  //err.statusCode = 404;
+  //err.status = 'failed';
+
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
